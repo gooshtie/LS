92ae7c98be98b22adb0f46bd54ff0f4913dce4d8
Chris Hennessy <curlyhennessymusic@gmail.com>
2023-05-29 21:12:03 -0400

    update file hard_1.rb

--- rb101-rb109spe/lesson_3/hard_1.rb
+++ rb101-rb109spe/lesson_3/hard_1.rb
@@ -1,8 +1,36 @@
 #1 nil
 
-greetings = { a: 'hi' }
-informal_greeting = greetings[:a]
-informal_greeting << ' there'
+# greetings = { a: 'hi' }
+# informal_greeting = greetings[:a]
+# informal_greeting << ' there'
 
-puts informal_greeting  #  => "hi there"
-puts greetings
+# puts informal_greeting  #  => "hi there"
+# puts greetings
+
+
+#2 { a: 'hi' }
+
+
+#3
+
+# A : one is one two is two three is three
+# B : one is one two is two three is three
+# C : one is two two is three three is one
+
+
+#4
+
+
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false unless dot_separated_words.size == 4
+
+  while dot_separated_words.size > 0  do
+    word = dot_separated_words.pop
+    break unless is_an_ip_number?(word)
+  end
+  return true
+end
+
+
+puts dot_separated_ip_address?("10.4.5.33")
